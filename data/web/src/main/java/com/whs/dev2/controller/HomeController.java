f7504378702dee0cb1b19a7dd325ac852eb9707b
yelin1197 <yelin1197@ewhain.net>
2025-06-20 09:52:37 +0900

    feat: 암호기능 추가, 도커 화면 띄우기까지 성공

--- data/web/src/main/java/com/whs/dev2/controller/HomeController.java
+++ data/web/src/main/java/com/whs/dev2/controller/HomeController.java
@@ -2,14 +2,12 @@ package com.whs.dev2.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.ui.Model;
 
 @Controller
 public class HomeController {
 
-
     @GetMapping("/")
-    public String home(Model model) {
+    public String home() {
         return "index";
     }
 }
