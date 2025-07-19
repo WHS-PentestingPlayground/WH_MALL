d5210da841f3bea4cd713ad22baa2e89bb852b06
meowyeok <vkdnjaldur@gmail.com>
2025-07-20 00:58:19 +0900

    [UPDATE] cron설정

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -2,17 +2,17 @@
 while true; do
   echo "Restarting web-server-1..."
   docker-compose restart web-server-1
-  sleep 1800
+  sleep 3600
   
   echo "Restarting web-server-2..."
   docker-compose restart web-server-2
-  sleep 1800
+  sleep 3600
   
   echo "Restarting api-server-1..."
   docker-compose restart api-server-1
-  sleep 3600
+  sleep 10800
   
   echo "Restarting api-server-2..."
   docker-compose restart api-server-2
-  sleep 3600
+  sleep 10800
 done
